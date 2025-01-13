60f306667443f6486aaed256e00d2f22ab60af07
brosdee21 <nwaokolodennis90@gmail.com>
2025-01-13 15:28:09 +0100

    s

--- Routes/routes.js
+++ Routes/routes.js
@@ -1,13 +1,3 @@
-// const express = require("express");
-// const router = express.Router();
-// const { sendContactEmail } = require("../controllers/user");
-
-// // Ensure the correct method (POST) and path (/api/contact)
-// router.post("/api/contact", sendContactEmail);
-
-// module.exports = router;
-
-
 const express = require("express");
 const { sendEmail } = require("../controllers/user");
 const router = express.Router();
--- models/user.js
+++ models/user.js
@@ -1,31 +1,3 @@
-// const mongoose = require('mongoose');
-
-// const userSchema = new mongoose.Schema({
-//     firstName: {
-//         type: String,
-//         required: true
-//     },
-//     lastName: {
-//         type: String,
-//         required: true
-//     },
-//    email: {
-//         type: String,
-//         required: true
-//     },
-//     phone: {
-//         type: String,
-//         required: true
-//     },
-//     message: {
-//         type: String,
-//         required: true
-//     }
-// });
-
-// module.exports = mongoose.model("User", userSchema);
-
-
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
